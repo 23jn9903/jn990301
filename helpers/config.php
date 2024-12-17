0c50c4b8397bd7217e0e3c3b8923d78c98b7098f
23jn9903 <23jn9903@jec.ac.jp>
2024-12-17 18:45:49 +0900

    “JecShoppingソースコード、CSSなど追加”　と入力

--- helpers/config.php
+++ helpers/config.php
@@ -1,5 +1,5 @@
 <?php
     // DB接続設定
-    define('DSN', 'sqlsrv:server=10.32.97.1;database=20jn0300');
-    define('DB_USER', '20jn0300');
-    define('DB_PASSWORD', '20jn0300');
+    define('DSN', 'sqlsrv:server=tcp:jn9903db.database.windows.net,1433;;database=jn990301');
+    define('DB_USER', 'jndb');
+    define('DB_PASSWORD', 'Pa$$word1234');
